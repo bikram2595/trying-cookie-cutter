07a759635d8cb0ada444ea465bb6e2794a7cb6fa
Bikram Das <bikramkumardas2512@gmail.com>
2023-11-26 19:43:57 +0530

    trying out cookie cutter for the first time

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Trying cookie cutter for the first time. Time to code according to the industry standards',
+    author='Bikram',
+    license='',
+)
